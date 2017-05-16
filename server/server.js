a596f91ed0532aad96139420669a8c3cd9b5a6f8
Abner Horn <Abner Horn>
2017-05-16 11:09:51 -0300

    functional version

--- server/server.js
+++ server/server.js
@@ -38,32 +38,38 @@ app.post('/upload',(req,res) => {
 
 })
 
+app.post('/cleardb',(req,res) => {
+  Place.remove({}).then(()=>{
+    res.send("DB cleared");
+  }).catch((e) => {
+    res.send(`Error : ${e}`)
+  })
+})
+
 app.get('/show/:lat/:lng',(req,res) => {
   var lat = req.params.lat;
   var lng = req.params.lng;
 
   Place.find().then((places)=>{
-    // var distance = (lat1, lon1, lat2, lon2) => {
-    //   var p = 0.017453292519943295;
-    //   var c = Math.cos;
-    //   var a = 0.5 - c((lat2 - lat1) * p)/2 + c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))/2;
-    //  return 12742 * Math.asin(Math.sqrt(a));
-    // };
-    //
-    // var showPlaces = (arr,x,y) => {
-    //   for (i in arr){
-    //     arr[i]._doc.distance = distance(x,y,arr[i].lat,arr[i].lng);
-    //   }
-    //   arr.sort(function(a, b) {
-    //     return parseFloat(a._doc.distance) - parseFloat(b._doc.distance);
-    //   })
-    //
-    //   return arr.splice(0,4);
-    // }
-    //
-    // res.status(200).send(showPlaces(places,lat,lng));
+    var distance = (lat1, lon1, lat2, lon2) => {
+      var p = 0.017453292519943295;
+      var c = Math.cos;
+      var a = 0.5 - c((lat2 - lat1) * p)/2 + c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))/2;
+     return 12742 * Math.asin(Math.sqrt(a));
+    };
 
-    res.status(200).send(places);
+    var showPlaces = (arr,x,y) => {
+      for (i in arr){
+        arr[i]._doc.distance = distance(x,y,arr[i].lat,arr[i].lng);
+      }
+      arr.sort(function(a, b) {
+        return parseFloat(a._doc.distance) - parseFloat(b._doc.distance);
+      })
+
+      return arr.splice(0,4);
+    }
+
+    res.status(200).send(showPlaces(places,lat,lng));
 
   }).catch((e) => {
     res.status(400).send(e);
